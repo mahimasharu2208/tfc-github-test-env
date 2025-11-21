bd3b26a6bda56bb5c2e5d1ab076177a6b373b036
Mahima <mahima.sharu@hashicorp.com>
2025-11-21 15:50:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ provider "aws" {
 
 # A simple resource: AWS S3 Bucket
 resource "aws_s3_bucket" "example" {
-  bucket = "tfc-github-test-bucket-unique-125" # MUST be globally unique
+  bucket = "tfc-github-test-bucket-unique-12345" # MUST be globally unique
   
   tags = {
     Name        = "Managed by TFC via GitHub Actions"
